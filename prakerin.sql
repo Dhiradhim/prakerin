cbe85c02ef26caaeeece02d2f46adf320f3c8a04
Dhiradhim <dhiradhim@gmail.com>
2021-10-23 09:18:22 +0800

    initial commit

--- prakerin.sql
+++ prakerin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 22, 2021 at 11:31 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Generation Time: Oct 23, 2021 at 03:18 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,10 +39,81 @@ CREATE TABLE `perusahaan` (
 --
 
 INSERT INTO `perusahaan` (`id`, `nama_perusahaan`, `alamat_perusahaan`, `no_telp`) VALUES
-(1, 'PT. Gojek Indonesia', 'jl. Soekarno Hatta  no 121', '(021) 7456352'),
-(2, 'PT. Tokopedia', 'jl. Kejora No. 32', '(021) 4625252'),
-(3, 'PT. Grab Indonesia', 'jl. Mayestik no 21', '(021) 8924231'),
-(4, 'PT. Ovo Payment', 'jl. Imam Bonjol No. 88', '(021) 7482648');
+(1, 'PT. Mencari Cinta Sejati', 'Jl. Patah Hati No 34', '(021) 1564138'),
+(2, 'PT. Tokopedia Indonesia', 'jl. Soekarno Hatta  no 121', '(021) 2613541'),
+(3, 'PT. Ovo Payment', 'Jl. Merdeka No 210', '(021) 6521872');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prakerin`
+--
+
+CREATE TABLE `prakerin` (
+  `id` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `id_siswa` int(10) NOT NULL,
+  `id_perusahaan` int(10) NOT NULL,
+  `tgl_mulai` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `status_prakerin` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prakerin`
+--
+
+INSERT INTO `prakerin` (`id`, `id_user`, `id_siswa`, `id_perusahaan`, `tgl_mulai`, `tgl_kembali`, `status_prakerin`) VALUES
+(7, 4, 4, 2, '2021-08-02', '2021-10-29', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `id_perusahaan` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `jurusan` varchar(10) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id`, `id_user`, `id_perusahaan`, `nama`, `jenis_kelamin`, `kelas`, `jurusan`, `status`) VALUES
+(4, 4, 2, 'Lillik Pratama S', 'Laki-laki', '11OTKP1', 'OTKP', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `jabatan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `jabatan`) VALUES
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'administrator'),
+(2, 'kepsek', 'e10adc3949ba59abbe56e057f20f883e', 'kepsek'),
+(3, 'kajur', 'e10adc3949ba59abbe56e057f20f883e', 'kajur'),
+(4, '1111', 'e10adc3949ba59abbe56e057f20f883e', 'siswa'),
+(5, '1112', 'e10adc3949ba59abbe56e057f20f883e', 'siswa'),
+(7, '1113', 'e10adc3949ba59abbe56e057f20f883e', 'siswa');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +126,24 @@ ALTER TABLE `perusahaan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `prakerin`
+--
+ALTER TABLE `prakerin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +151,25 @@ ALTER TABLE `perusahaan`
 -- AUTO_INCREMENT for table `perusahaan`
 --
 ALTER TABLE `perusahaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `prakerin`
+--
+ALTER TABLE `prakerin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
